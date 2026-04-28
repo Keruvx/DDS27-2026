b39d435d1bbea204b238b75a5590cd8691dbf412
Keruvx <jhonys_s2@hotmail.com>
2026-04-28 19:29:14 -0300

    AulasDDS27

---
+++ Atv1.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Atv1 {
+        int idade = 0;
+        
+    Scanner scan = new Scanner(System.in);
+    
+System.out.println("Escreva sua idade: ");
+idade = scan.nextInt();
+    
+if (idade < 16){
+     System.out.println("Não pode Votar!");
+    }
+if (idade == 16) || (idade == 17) || (idade > 65){
+        System.out.println("Seu Voto e facultativo");
+}   
+if (idade >= 18) && (idade <= 65){
+        System.out.println("Voce deve voltar");
+}
+    else{
+System.out.println("Idade Invalida");
+}
+}
+}
